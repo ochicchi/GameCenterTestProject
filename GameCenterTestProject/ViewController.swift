19bb5f04de7d0affa400fa701ada28c499cf66cd
ochicchi <s.ohguchi@gmail.com>
2016-03-09 09:11:00 +0900

    Initial Commit

---
+++ GameCenterTestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GameCenterTestProject
+//
+//  Created by 大口 聡 on 2016/03/09.
+//  Copyright © 2016年 maripara.org. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
